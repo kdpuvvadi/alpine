d9628bacb3f06c3b09c00cf419f6f958a9c7015a
KD Puvvadi <rao@puvvadi.me>
2023-02-05 21:38:27 +0530

    build pswh

---
+++ build.ps1
@@ -0,0 +1,20 @@
+$Reg=Read-Host "Choose a Registry code ghcr.io(g) or Docker Hub(d)?"
+Switch ($Reg)
+{
+    g {$IMAGEPATH="ghcr.io/kdpuvvadi/alpine"}
+    d {$IMAGEPATH="kdpuvvadi/alpine"}
+}
+
+$VERSIONS = @(
+    3.11
+    3.12
+    3.13
+    3.14
+    3.15
+    3.16
+    3.17
+    "latest"
+)
+foreach ( $VERSION in $VERSIONS ) {
+    docker buildx build --platform="linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6" --pull --push --build-arg IMAGE_TAG=${VERSION} -t ${IMAGEPATH}:${VERSION} .
+}
